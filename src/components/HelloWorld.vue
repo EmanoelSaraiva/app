7089a12d2aaf997e33420c70b5f497133aa2c73e
Emanoel <emanoelsaraivas@gmail.com>
2024-04-03 09:44:56 -0300

    Submit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,8 +3,15 @@
     <form>
       <label>Nome</label><br />
       <input type="text" v-model="name" />
-      <!-- Diretivas do Vue onde posso subistituir o v-on por @ e também usar o método .prevent() para evitar o reload da página na submissão do formulário -->
+      <!-- Diretivas do Vue onde posso subistituir o v-on por @ e também usar o modificador .prevent() para evitar o reload da página na submissão do formulário -->
       <button @click.prevent="onClick(name)">Enviar</button>
+    </form>
+    <br />
+    <!-- Outra forma de se fazer é com submit no form's, usando o .prevent() que é um modificador de ação-->
+    <form @submit.prevent="onSubmit(name2)">
+      <label>Nome</label><br />
+      <input type="text" v-model="name2" />
+      <button type="submit">Enviar</button>
     </form>
   </div>
 </template>
@@ -15,11 +22,15 @@ export default {
   data() {
     return {
       name: "",
+      name2: "",
     };
   },
   methods: {
     onClick(name) {
       console.log("Hello World " + name);
+    },
+    onSubmit(name2) {
+      console.log("Hello World " + name2);
     },
   },
 };
